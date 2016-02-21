041868eb54f197a83bf82240454c4b5bb998da26
Bruno Almeida <Bruno@Brunos-MacBook-Air.local>
2016-02-20 23:52:11 -0500

    Add logs
    Also adds test code (commented out).

--- Android/FRC2016Scouting/app/src/main/java/com/example/brunoalmeida/frc2016scouting/MainActivity.java
+++ Android/FRC2016Scouting/app/src/main/java/com/example/brunoalmeida/frc2016scouting/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.brunoalmeida.frc2016scouting;
 
+import android.app.Fragment;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,4 +54,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
--- Android/FRC2016Scouting/app/src/main/java/com/example/brunoalmeida/frc2016scouting/MainActivityFragment.java
+++ Android/FRC2016Scouting/app/src/main/java/com/example/brunoalmeida/frc2016scouting/MainActivityFragment.java
@@ -1,7 +1,14 @@
 package com.example.brunoalmeida.frc2016scouting;
 
+//import android.app.ActionBar;             // modern action bar
+import android.app.Activity;
+import android.net.Uri;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;    // support action bar
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -37,11 +44,36 @@ public class MainActivityFragment extends Fragment implements View.OnClickListen
             case R.id.new_profile:
                 newProfile();
                 break;
+            default:
+                Log.w(LOG_TAG, "onClick not handled");
         }
     }
 
     private void newProfile() {
         Log.v(LOG_TAG, "in newProfile()");
+
+        // Test - Replace one fragment with another
+        /*getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment, new NewProfileFragment())
+                .addToBackStack(null)
+                .commit();*/
+
+
+        // Test - Set the ActionBar's title from a Fragment
+/*        AppCompatActivity a = (AppCompatActivity) getActivity();
+
+        if (a != null) {
+            ActionBar ab = a.getSupportActionBar();
+
+            if (ab != null) {
+                ab.setTitle("Test");
+            } else {
+                Log.w(LOG_TAG, "action bar is null");
+            }
+        } else {
+            Log.w(LOG_TAG, "activity is null");
+        }*/
     }
 
 }
