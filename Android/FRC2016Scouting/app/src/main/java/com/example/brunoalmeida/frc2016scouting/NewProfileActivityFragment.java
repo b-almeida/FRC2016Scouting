72753569f417e72bb97b81e4f439b81c744fdab2
Bruno Almeida <Bruno@Brunos-MacBook-Air.local>
2016-02-21 00:08:15 -0500

    Add NewProfileActivity and fix strings
    - Add a string for the main activity, instead of using the app name.
    - Add a NewProfileActivity

---
+++ Android/FRC2016Scouting/app/src/main/java/com/example/brunoalmeida/frc2016scouting/NewProfileActivityFragment.java
@@ -0,0 +1,23 @@
+package com.example.brunoalmeida.frc2016scouting;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class NewProfileActivityFragment extends Fragment {
+
+    public NewProfileActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_new_profile, container, false);
+    }
+
+}
