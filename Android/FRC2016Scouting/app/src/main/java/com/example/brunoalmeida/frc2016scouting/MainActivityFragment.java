65f1dba4d752a1bcb28b22e39ecf42dfa1e8c26d
Bruno Almeida <Bruno@Brunos-MacBook-Air.local>
2016-02-20 21:44:31 -0500

    Add new profile button in MainActivityFragment

--- Android/FRC2016Scouting/app/src/main/java/com/example/brunoalmeida/frc2016scouting/MainActivityFragment.java
+++ Android/FRC2016Scouting/app/src/main/java/com/example/brunoalmeida/frc2016scouting/MainActivityFragment.java
@@ -2,21 +2,46 @@ package com.example.brunoalmeida.frc2016scouting;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment implements View.OnClickListener {
+
+    private static final String LOG_TAG = "MainActivityFragment";
 
     public MainActivityFragment() {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        Button newProfile = (Button) view.findViewById(R.id.new_profile);
+        newProfile.setOnClickListener(this);
+
+        return view;
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.new_profile:
+                newProfile();
+                break;
+        }
+    }
+
+    private void newProfile() {
+        Log.v(LOG_TAG, "in newProfile()");
+    }
+
 }
