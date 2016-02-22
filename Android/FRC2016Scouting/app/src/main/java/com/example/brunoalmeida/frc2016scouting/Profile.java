6c76ab69d304673417a0077e172c09d02c8823fb
Bruno Almeida <Bruno@Brunos-MacBook-Air.local>
2016-02-21 19:15:59 -0500

    Create Profile class

---
+++ Android/FRC2016Scouting/app/src/main/java/com/example/brunoalmeida/frc2016scouting/Profile.java
@@ -0,0 +1,27 @@
+package com.example.brunoalmeida.frc2016scouting;
+
+/**
+ * A profile of a single team and robot, with information on both the team and robot.
+ *
+ * Created by Bruno on 2016-02-21.
+ */
+public class Profile {
+
+    private int teamNumber;
+    private String robotFunction;
+
+
+    public Profile(int teamNumber, String robotFunction) {
+        this.teamNumber = teamNumber;
+        this.robotFunction = robotFunction;
+    }
+
+    public int getTeamNumber() {
+        return teamNumber;
+    }
+
+    public String getRobotFunction() {
+        return robotFunction;
+    }
+
+}
