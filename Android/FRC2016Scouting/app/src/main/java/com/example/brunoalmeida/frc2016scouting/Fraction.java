427348e36b5409e7973fbfa22d2a9cc4b8893c3b
Bruno Almeida <Bruno@Brunos-MacBook-Air.local>
2016-02-25 20:22:54 -0500

    Add Fraction class

---
+++ Android/FRC2016Scouting/app/src/main/java/com/example/brunoalmeida/frc2016scouting/Fraction.java
@@ -0,0 +1,10 @@
+package com.example.brunoalmeida.frc2016scouting;
+
+/**
+ * Represents a fraction, with a numerator and denominator.
+ * Can be represented as a normal fraction or a percentage.
+ *
+ * Created by Bruno on 2016-02-25.
+ */
+public class Fraction {
+}
