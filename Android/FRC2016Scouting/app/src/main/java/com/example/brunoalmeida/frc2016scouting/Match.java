9137971f9805aa269bef7ba5aed0605c56c85a0b
Bruno Almeida <Bruno@Brunos-MacBook-Air.local>
2016-02-25 20:29:59 -0500

    Add Match fields

--- Android/FRC2016Scouting/app/src/main/java/com/example/brunoalmeida/frc2016scouting/Match.java
+++ Android/FRC2016Scouting/app/src/main/java/com/example/brunoalmeida/frc2016scouting/Match.java
@@ -7,4 +7,21 @@ package com.example.brunoalmeida.frc2016scouting;
  * Created by Bruno on 2016-02-25.
  */
 public class Match {
+
+    private int teamNumber;
+    private int ally1TeamNumber;
+    private int ally2TeamNumber;
+    private int opponent1TeamNumber;
+    private int opponent2TeamNumber;
+    private int opponent3TeamNumber;
+
+    private Fraction lowShootingSuccess;
+    private Fraction highShootingSuccess;
+
+    private Fraction defenseLowBarBreachSuccess;
+    private Fraction defenseCategoryABreachSuccess;
+    private Fraction defenseCategoryBBreachSuccess;
+    private Fraction defenseCategoryCBreachSuccess;
+    private Fraction defenseCategoryDBreachSuccess;
+
 }
