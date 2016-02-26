aa5e64f890056d7e93f96e0c16a9636ccd40bc8b
Bruno Almeida <Bruno@Brunos-MacBook-Air.local>
2016-02-25 20:25:57 -0500

    Implement Fraction class

--- Android/FRC2016Scouting/app/src/main/java/com/example/brunoalmeida/frc2016scouting/Fraction.java
+++ Android/FRC2016Scouting/app/src/main/java/com/example/brunoalmeida/frc2016scouting/Fraction.java
@@ -2,9 +2,39 @@ package com.example.brunoalmeida.frc2016scouting;
 
 /**
  * Represents a fraction, with a numerator and denominator.
- * Can be represented as a normal fraction or a percentage.
+ * Can be represented as a normal fraction or a percent.
  *
  * Created by Bruno on 2016-02-25.
  */
 public class Fraction {
+
+    private int numerator;
+    private int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public int getPercent() {
+        return (numerator * 100) / denominator;
+    }
+
+    @Override
+    public String toString() {
+        return numerator + "/" + denominator;
+    }
+
+    public String toPercent() {
+        return getPercent() + "%";
+    }
+
 }
