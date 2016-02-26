0ce8d42a6e104c218bc80c223586bde73786bed9
Bruno Almeida <Bruno@Brunos-MacBook-Air.local>
2016-02-25 20:12:26 -0500

    Add Match class

---
+++ Android/FRC2016Scouting/app/src/main/java/com/example/brunoalmeida/frc2016scouting/Match.java
@@ -0,0 +1,10 @@
+package com.example.brunoalmeida.frc2016scouting;
+
+/**
+ * The data for one robot's match.
+ * Includes the robot, its allies, and its statistics.
+ *
+ * Created by Bruno on 2016-02-25.
+ */
+public class Match {
+}
