f7873357119406f5068419bae8bfcbcc3009ef22
Bruno Almeida <Bruno@Brunos-MacBook-Air.local>
2016-02-25 21:08:44 -0500

    Prepare to write match table to database
    NewMatchFragment:
    - Rename the EditText fields.
    - Get the EditText values and construct a Match object.
    - Create a writeMatchToDB() method skeleton.
    
    ProfileDBHelper:
    - Add create and drop match table operations.

--- Android/FRC2016Scouting/app/src/main/java/com/example/brunoalmeida/frc2016scouting/NewMatchActivity.java
+++ Android/FRC2016Scouting/app/src/main/java/com/example/brunoalmeida/frc2016scouting/NewMatchActivity.java
@@ -1,6 +1,8 @@
 package com.example.brunoalmeida.frc2016scouting;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +10,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.example.brunoalmeida.frc2016scouting.database.ProfileContract;
+import com.example.brunoalmeida.frc2016scouting.database.ProfileDBHelper;
 
 public class NewMatchActivity extends AppCompatActivity {
 
@@ -32,13 +39,66 @@ public class NewMatchActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    /**
+     * @param id EditText view id.
+     */
+    private int getTeamNumberFromEditText(int id) {
+        String teamNumberString = ( (EditText) findViewById(R.id.team_number) )
+                .getText().toString();
+
+        if (teamNumberString != null && teamNumberString.length() > 0) {
+            return Integer.parseInt(teamNumberString);
+        } else {
+            return 0;
+        }
+    }
+
     public void createMatchOnClick(View view) {
         Log.v(LOG_TAG, "in createMatchOnClick()");
+
+        int teamNumber = getTeamNumberFromEditText(R.id.team_number);
+        int ally1TeamNumber = getTeamNumberFromEditText(R.id.ally_1_team_number);
+        int ally2TeamNumber = getTeamNumberFromEditText(R.id.ally_2_team_number);
+        int opponent1TeamNumber = getTeamNumberFromEditText(R.id.opponent_1_team_number);
+        int opponent2TeamNumber = getTeamNumberFromEditText(R.id.opponent_2_team_number);
+        int opponent3TeamNumber = getTeamNumberFromEditText(R.id.opponent_3_team_number);
+
+        Match match = new Match(
+                teamNumber,
+                ally1TeamNumber,
+                ally2TeamNumber,
+                opponent1TeamNumber,
+                opponent2TeamNumber,
+                opponent3TeamNumber);
+
+        // Write the data to the database
+        writeMatchToDB(match);
+
 
         // Switch to MatchActivity
         Intent intent = new Intent(this, MatchActivity.class);
         startActivity(intent);
         Log.v(LOG_TAG, "Starting MatchActivity");
+    }
+
+    private void writeMatchToDB(Match match) {
+        ProfileDBHelper profileDBHelper = new ProfileDBHelper(this);
+
+        // Gets the data repository in write mode
+        SQLiteDatabase database = profileDBHelper.getWritableDatabase();
+
+        // Create a new map of values, where column names are the keys
+        ContentValues values = new ContentValues();
+        //values.put(ProfileContract.ProfileEntry.COLUMN_TEAM_NUMBER, teamNumber);
+        //values.put(ProfileContract.ProfileEntry.COLUMN_ROBOT_TYPE, robotType);
+
+        // Insert the new row, returning the primary key value of the new row
+        long newRowID = database.insert(
+                ProfileContract.MatchEntry.TABLE_NAME,
+                null,
+                values);
+
+        Log.v(LOG_TAG, "writeProfileToDB(): newRowID = " + newRowID);
     }
 
 }
--- Android/FRC2016Scouting/app/src/main/java/com/example/brunoalmeida/frc2016scouting/database/ProfileDBHelper.java
+++ Android/FRC2016Scouting/app/src/main/java/com/example/brunoalmeida/frc2016scouting/database/ProfileDBHelper.java
@@ -18,7 +18,7 @@ public class ProfileDBHelper extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
 
 
-    // SQL operations
+    // Profile Table - SQL operations
     private static final String SQL_CREATE_PROFILE_TABLE =
             "CREATE TABLE " + ProfileEntry.TABLE_NAME +
                     " (" +
@@ -29,6 +29,42 @@ public class ProfileDBHelper extends SQLiteOpenHelper {
 
     private static final String SQL_DROP_PROFILE_TABLE =
             "DROP TABLE IF EXISTS " + ProfileEntry.TABLE_NAME;
+
+
+    // Match Table - SQL operations
+    private static final String SQL_CREATE_MATCH_TABLE =
+            "CREATE TABLE " + MatchEntry.TABLE_NAME +
+                    " (" +
+                    MatchEntry._ID                              + " INTEGER PRIMARY KEY," +
+
+                    MatchEntry.COLUMN_TEAM_NUMBER               + " INTEGER," +
+                    MatchEntry.COLUMN_ALLY_1_TEAM_NUMBER        + " INTEGER," +
+                    MatchEntry.COLUMN_ALLY_2_TEAM_NUMBER        + " INTEGER," +
+                    MatchEntry.COLUMN_OPPONENT_1_TEAM_NUMBER    + " INTEGER," +
+                    MatchEntry.COLUMN_OPPONENT_2_TEAM_NUMBER    + " INTEGER," +
+                    MatchEntry.COLUMN_OPPONENT_3_TEAM_NUMBER    + " INTEGER," +
+
+                    MatchEntry.COLUMN_LOW_SHOOTING_SUCCESSES    + " INTEGER," +
+                    MatchEntry.COLUMN_LOW_SHOOTING_ATTEMPTS     + " INTEGER," +
+                    MatchEntry.COLUMN_HIGH_SHOOTING_SUCCESSES   + " INTEGER," +
+                    MatchEntry.COLUMN_HIGH_SHOOTING_ATTEMPTS    + " INTEGER," +
+
+                    MatchEntry.COLUMN_DEFENSE_LOW_BAR_BREACH_SUCCESSES      + " INTEGER," +
+                    MatchEntry.COLUMN_DEFENSE_LOW_BAR_BREACH_ATTEMPTS       + " INTEGER," +
+                    MatchEntry.COLUMN_DEFENSE_CATEGORY_A_BREACH_SUCCESSES   + " INTEGER," +
+                    MatchEntry.COLUMN_DEFENSE_CATEGORY_A_BREACH_ATTEMPTS    + " INTEGER," +
+                    MatchEntry.COLUMN_DEFENSE_CATEGORY_B_BREACH_SUCCESSES   + " INTEGER," +
+                    MatchEntry.COLUMN_DEFENSE_CATEGORY_B_BREACH_ATTEMPTS    + " INTEGER," +
+                    MatchEntry.COLUMN_DEFENSE_CATEGORY_C_BREACH_SUCCESSES   + " INTEGER," +
+                    MatchEntry.COLUMN_DEFENSE_CATEGORY_C_BREACH_ATTEMPTS    + " INTEGER," +
+                    MatchEntry.COLUMN_DEFENSE_CATEGORY_D_BREACH_SUCCESSES   + " INTEGER," +
+                    MatchEntry.COLUMN_DEFENSE_CATEGORY_D_BREACH_ATTEMPTS    + " INTEGER" +
+                    ")";
+
+    private static final String SQL_DROP_MATCH_TABLE =
+            "DROP TABLE IF EXISTS " + MatchEntry.TABLE_NAME;
+
+
 
 
     public ProfileDBHelper(Context context) {
