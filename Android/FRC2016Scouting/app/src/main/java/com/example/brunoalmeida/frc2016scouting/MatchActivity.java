a7f4a0ec9d3aba24b315626ceded50e955fff45d
Bruno Almeida <Bruno@Brunos-MacBook-Air.local>
2016-03-01 20:43:27 -0500

    Remove save menu item

--- Android/FRC2016Scouting/app/src/main/java/com/example/brunoalmeida/frc2016scouting/MatchActivity.java
+++ Android/FRC2016Scouting/app/src/main/java/com/example/brunoalmeida/frc2016scouting/MatchActivity.java
@@ -97,11 +97,6 @@ public class MatchActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        if (id == R.id.action_save) {
-            //startProfileActivity();
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
