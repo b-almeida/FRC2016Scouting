65c432112b649d87dcc59654e938d048b5ff7e65
Bruno Almeida <Bruno@Brunos-MacBook-Air.local>
2016-03-05 16:16:02 -0500

    Add DataExport class

---
+++ Android/FRC2016Scouting/app/src/main/java/com/example/brunoalmeida/frc2016scouting/database/DataExport.java
@@ -0,0 +1,8 @@
+package com.example.brunoalmeida.frc2016scouting.database;
+
+/**
+ * Exports data from the database to files in public directories on the device.
+ * Created by Bruno on 2016-03-05.
+ */
+public class DataExport {
+}
