1e0f7cfb93b7168f7f42ebbc5e1932800b202bbb
Bruno Almeida <Bruno@Brunos-MacBook-Air.local>
2016-03-10 09:02:53 -0500

    Cleanup

--- Android/FRC2016Scouting/app/src/main/java/com/example/brunoalmeida/frc2016scouting/ExportDataActivity.java
+++ Android/FRC2016Scouting/app/src/main/java/com/example/brunoalmeida/frc2016scouting/ExportDataActivity.java
@@ -1,21 +1,17 @@
 package com.example.brunoalmeida.frc2016scouting;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
 
 import com.example.brunoalmeida.frc2016scouting.data.Match;
@@ -342,8 +338,6 @@ public class ExportDataActivity
     }
 
     private void writeMatchDataToCSVFiles(File directory) {
-        /* Export profiles (match data) in separate files */
-
         try {
 
             // Write match data (loop through each profile for its matches)
